5ceb4ff429d0dce9d68257e9e264f690a1b88a1e
atillagallio <atilla.gallio@gmail.com>
2018-07-29 09:54:53 -0300

    main menu finished

--- GameJamplus/Assets/Scripts/Managers/GameOverUIManager.cs
+++ GameJamplus/Assets/Scripts/Managers/GameOverUIManager.cs
@@ -25,6 +25,9 @@ public class GameOverUIManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if(Input.GetButtonDown("Submit")){
+			UnityEngine.SceneManagement.SceneManager.LoadScene("MainMenu");
+		}
 		
 	}
 }
--- GameJamplus/Assets/Scripts/Managers/MainMenuManager.cs
+++ GameJamplus/Assets/Scripts/Managers/MainMenuManager.cs
@@ -5,24 +5,34 @@ using TMPro;
 
 public class MainMenuManager : MonoBehaviour {
 
-	public TextMeshProUGUI playerCount;
+	public TextMeshPro playerCount;
 	public List<GameObject> controllers;
 
 	public int numberOfControllers = 0;
 	// Use this for initialization
 	void Start () {
 
-		
 	}
 	
 	// Update is called once per frame
+	void TurnOffAllControllers(){
+		foreach (GameObject controller in controllers){
+			controller.SetActive(false);
+		}
+	}
+
 	void Update () {
 		int i= 0;
+		if(Input.GetButtonDown("Submit")){
+			UnityEngine.SceneManagement.SceneManager.LoadScene("Gameplay 1");
+		}
 		if(numberOfControllers != Input.GetJoystickNames().Length){
+			TurnOffAllControllers();
 			foreach (string name in Input.GetJoystickNames()){
 				controllers[i].SetActive(true);
 				i++;
 			}
+			playerCount.text = i + " Players";
 		}	 
 	}
 }
